3a39d959c056a929934e8a1cac6c7ca6865fd6ee
prantoran <prantoran@gmail.com>
2019-05-15 16:16:27 +0600

    debugging

--- Codejam/2019/1B/solution.cpp
+++ Codejam/2019/1B/solution.cpp
@@ -8,9 +8,9 @@
 using namespace std;
 
 vector<int> c, d;
+vector<vector<int> > rmqC, rmqD;
 
 int inv2[MX];
-int rmq[20][MX];
 
 void setInv2() {
     int base = 1;
@@ -21,17 +21,21 @@ void setInv2() {
     }
 }
 
-void setRMQ(int n, const vector<int>& arr) {
-    memset(rmq, 0, sizeof rmq);
+void setRMQ(int n, const vector<int>& arr, vector<vector<int> >& rmq) {
+    int ln = inv2[n] + 1;
+
+    rmq = vector<vector<int> > (ln);
+
+    rmq[0] = vector<int> (n);
 
     for (int j = 0; j < n; j ++) {
         rmq[0][j] = arr[j];
     }
 
-    int ln = inv2[n] + 1;
 
     int base = 1;
     for (int i = 1; i < ln; i ++) {
+        rmq[i] = vector<int> (n - 2*base + 1);
         for (int j = 0; j + 2*base -1 < n; j ++) {
             rmq[i][j] = max(rmq[i-1][j], rmq[i-1][j+base]);
         }
@@ -50,21 +54,29 @@ void setRMQ(int n, const vector<int>& arr) {
     }
 }
 
-int query(int l, int r) {
+int query(int l, int r, const vector<vector<int> > rmq) {
     int range = r-l+1;
     int ln = inv2[range];
     int base = 1 << ln;
+
+    cout << "query() range:" << range << " l:" << l << " r:" << r << " base:" << base << " ln:" << ln << " r-base+1:" << r-base+1 << " lft:" << rmq[ln][l] << " rgt:" << rmq[ln][r-base+1] << endl;
     return max(rmq[ln][l], rmq[ln][r-base+1]);
 }
 
 bool p1(int l, int r, int v) {
-   if (query(l, r) <= v) return true;
+    cout << "p1() l:" << l << " r:" << r << " v:" << v << " query():" << query(l, r, rmqC) << endl;
+   if (query(l, r, rmqC) <= v) {
+       cout << "\ttrue\n";
+
+       return true;
+   }
+   cout << "\tfalse\n";
    return false; 
 }
 
 bool p2(int l, int r, int v, int k) {
     cout << "p2() l:" << l << " r:" << r << " v:" << v << " k:" << k << endl;
-    if (query(l, r) <= v + k) {
+    if (query(l, r, rmqD) <= v + k) {
         cout << "\ttrue\n";
         return true;
     }
@@ -74,7 +86,7 @@ bool p2(int l, int r, int v, int k) {
 }
 
 bool p3(int l, int r, int v, int k) {
-    if (query(l, r) < v - k) return true;
+    if (query(l, r, rmqD) < v - k) return true;
     return false;
 }
 
@@ -122,9 +134,9 @@ LL binarySearchIntervals(int i, int lastpos, int v, int k, bool (*px)(int l, int
     return ret;
 }
 
-LL intervalsCWillWin(int i, int mxlen, int v, int k) {
+LL intervalsCCanWin(int i, int mxlen, int v, int k) {
     LL ret = binarySearchIntervals(i, mxlen, v, k, p2);
-    cout << "intervalsCWillWin() ret:" << ret << endl;
+    cout << "intervalsCCanWin() ret:" << ret << endl;
     return ret;
 }
 
@@ -157,20 +169,19 @@ int main() {
             scanf("%d", &d[i]);
         }
 
-        setRMQ(n, d);
+        setRMQ(n, d, rmqD);
+        setRMQ(n, c, rmqC);
 
-        cout << query(1, max(1, n-3) ) << endl;
+        cout << query(1, max(1, n-3), rmqC) << endl;
 
         LL ans = 0;
         for (int i = 0 ; i < n; i ++) {
-            LL cur = intervalsCWillWin(i, n-1, c[i], k) - intervalsDWillLoose(i, n-1, c[i], k);
+            LL cur = intervalsCCanWin(i, n-1, c[i], k) - intervalsDWillLoose(i, n-1, c[i], k);
             ans += cur;
         }
 
         printf("Case #%d: %lld\n", ca, ans);
-
     }
-
 
     return 0;
 }
