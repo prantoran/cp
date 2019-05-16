8014bf4561f0f45b608585761ec8686491416b5d
Pinku Deb Nath <prantoran@gmail.com>
2019-05-16 23:47:27 +0600

    works

--- Codejam/2019/1A/Alien-Rhyme/solution.cpp
+++ Codejam/2019/1A/Alien-Rhyme/solution.cpp
@@ -1,97 +1,123 @@
+// https://codingcompetitions.withgoogle.com/codejam/round/0000000000051635/0000000000104e05
+
 #include <iostream>
 #include <stack>
+#include <cstring>
+#include <algorithm>
+
 using namespace std;
 
 
-class node {
+class Node {
     
     public:
-    node* parent;
-    node* children[26];
+    Node* parent;
+    Node* children[26];
     int cnt;
     int used;
-    int leaf;
-
     
-    node() {
+    Node() {
         this->parent = nullptr;
         this->cnt = 0;
-        this->leaf = 0;
         this->used = 0;
-        memset(this->children, NULL, sizeof this->children);
-    }
 
-    ~node() {
-        // destructor
+        for (int i = 0 ; i < 26; i ++) 
+            this->children[i] = nullptr;
     }
 };
 
 
-class trie {
+class Trie {
     public:
-    node* root;
+    Node* root;
 
-    trie() {
-        this->root = new node();
+    Trie() {
+        this->root = new Node();
+
     }
 
-    void insert(string s) {
-        node *cur =this->root;
-        int n = s.length();
+    void insert(char *s) {
+        Node *cur =this->root;
+        int n = strlen(s);
         for (int i = 0; i < n; i ++) {
             int id = s[i]-'A';
             if (cur->children[id] == nullptr) {
-                cur->children[id] = new node();
+                cur->children[id] = new Node();
                 cur->children[id]->parent = cur;
             }
 
-            cur->cnt ++;
-
             cur = cur->children[id];
+            cur->cnt ++;
         }
-        
-        cur->leaf ++;
     }
 
-    int countPairs() {
+    int countPairs(Node* cur) {
 
-        stack<node*> st;
+        if (cur->cnt < 2) return 0;
 
-        st.push(this->root);
-        int ans = 0;
+        int ret = 0;
 
-        while(!st.empty()) {
-            node* cur = st.top();
+        for (int i = 0 ; i < 26; i ++ ) {
+            Node* child = cur->children[i];
 
-            if (cur->cnt < 2) {
-                continue;
-            }
+            if (child == nullptr) continue;
+            if (child->cnt < 2) continue;
 
-            bool fnd = false;
+            ret += countPairs(child);
 
-            for (int i = 0 ; i < 26; i ++) {
-                node* child = cur->children[i];
-                
-                if (child == nullptr) continue;
-                if (child->cnt < 2) continue;
-                fnd = true;
-                st.push(child);
-
-            }
-
-            if (!fnd) {
-                ans += 2;
-                cur->cnt -= 2;
-                cur->parent->used += 2;
-            }
-
+            cur->used += child->used;
         }
+
+        cur->cnt -= cur->used;
+
+        if (cur->cnt >= 2) {
+            ret += 2;
+            cur->cnt -= 2;
+            cur->used += 2;
+        }
+
+        return ret;
+    }
+
+
+    int solve() {
+        int ret = 0;
+        for (int i=  0; i < 26; i ++) {
+            Node * child = this->root->children[i];
+            if (child == nullptr) continue;
+            ret += this->countPairs(child);
+        }
+
+        return ret;
     }
 
 };
 
 int main() {
     freopen("data.in", "r", stdin);
+
+    int t;
+    scanf("%d", &t);
+
+    for (int ca = 1; ca <= t; ca ++) {
+
+        Trie *trie = new Trie();
+
+        char s[100];
+
+        int n;
+        scanf("%d ", &n);
+
+        for (int i = 0 ; i < n; i ++) {
+            scanf("%s", s);
+            reverse(s, s + strlen(s));
+            trie->insert(s);
+        }
+
+        int ans = trie->solve();
+
+        printf("Case #%d: %d\n", ca, ans);
+    }
 
     return 0;
 }
