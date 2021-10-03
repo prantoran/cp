55f4e5d143a80e46903e8ecbb177fb4372badaa4
Pinku Deb Nath <prantoran@gmail.com>
2021-10-02 17:57:09 -0700

    Create unionfind.cpp

---
+++ unionfind.cpp
@@ -0,0 +1,39 @@
+class UnionFind {
+public:
+    UnionFind(int sz) : root(sz), rank(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+private:
+    vector<int> root;
+    vector<int> rank;
+};
