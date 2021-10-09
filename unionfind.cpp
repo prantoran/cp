7125103bb8c2ae60b78246435c914abf57b56890
Pinku Deb Nath <prantoran@gmail.com>
2021-10-09 01:57:01 -0700

    Update unionfind.cpp

--- unionfind.cpp
+++ unionfind.cpp
@@ -1,6 +1,6 @@
 class UnionFind {
 public:
-    UnionFind(int sz) : root(sz), rank(sz) {
+    UnionFind(int sz) : root(sz), rank(sz), c(sz) {
         for (int i = 0; i < sz; i++) {
             root[i] = i;
             rank[i] = 1;
@@ -26,12 +26,16 @@ public:
                 root[rootY] = rootX;
                 rank[rootX] += 1;
             }
+            
+            c --;
         }
     }
 
     bool connected(int x, int y) {
         return find(x) == find(y);
     }
+    
+    int components() { return c; }
 
 private:
     vector<int> root;
