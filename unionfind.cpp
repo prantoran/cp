08aead710f5274a0af0ffed1761ad318ed73b189
Pinku Deb Nath <prantoran@gmail.com>
2025-07-19 18:02:56 -0700

    Update unionfind.cpp

--- unionfind.cpp
+++ unionfind.cpp
@@ -42,3 +42,31 @@ private:
     vector<int> rank;
     int c;
 };
+
+
+class UnionFind {
+public:
+    vector<int> rank, root;
+    UnionFind(int n): rank(n, 1), root(n) {
+        iota(root.begin(), root.end(), 0);
+    }
+    int find(int u) {
+        return root[u] == u ? u : root[u] = find(root[u]);
+    }
+    bool unionSet(int u, int v) {
+        int ru = find(x), rv = find(y);
+        if (ru == rv) return false;
+        if (rank[ru] > rank[rv]) {
+            root[rv] = ru;
+        } else if (rank[ru] < rank[rv]) {
+            root[ru] = rv;
+        } else {
+            root[ru] = rv;
+            root[rv] ++;
+        }
+        return true;
+    }
+    bool isConnected(int u, int v) {
+        return find(u) == find(v);
+    }
+};
